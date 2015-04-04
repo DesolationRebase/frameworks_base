b92e17166d78cff7ccadce13311f7aba2686f01d
Dave Kessler <activethrasher00@gmail.com>
2015-04-04 15:47:24 +0200

    Add membar to recents options [1/2]
    Change-Id: Id1e799729543e385767ec8b68b126a20c4874baf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3476,6 +3476,12 @@ public final class Settings {
          */
         public static final String CUSTOM_QS_CARRIER_NAME = "custom_qs_carrier_name";
 
+	/**
+         * show the membar in recents
+         *  @hide
+         */
+        public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
