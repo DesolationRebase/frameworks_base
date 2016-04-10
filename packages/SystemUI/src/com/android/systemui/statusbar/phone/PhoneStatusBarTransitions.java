TeamBliss-LP/android_frameworks_base

Prevent NPE in status bar on alpha animation
Change-Id: Id16f584227c35540f4a6843007da1882f4653165

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -67,6 +67,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     }
 
     public ObjectAnimator animateTransitionTo(View v, float toAlpha) {
+        if (v == null) return null;
         return ObjectAnimator.ofFloat(v, "alpha", v.getAlpha(), toAlpha);
     }
 
@@ -118,15 +119,15 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             anims.start();
             mCurrentAnimation = anims;
         } else {
-            mLeftSide.setAlpha(newAlpha);
-            mStatusIcons.setAlpha(newAlpha);
-            mSignalCluster.setAlpha(newAlpha);
-            mNetworkTraffic.setAlpha(newAlpha);
-            mBattery.setAlpha(newAlphaBC);
-            mClock.setAlpha(newAlphaBC);
-            mCenterClock.setAlpha(newAlphaBC);
-            mLeftClock.setAlpha(newAlphaBC);
-            mBlissLogo.setAlpha(newAlphaBC);
+            if (mLeftSide != null) mLeftSide.setAlpha(newAlpha);
+            if (mStatusIcons != null) mStatusIcons.setAlpha(newAlpha);
+            if (mSignalCluster != null) mSignalCluster.setAlpha(newAlpha);
+            if (mNetworkTraffic != null) mNetworkTraffic.setAlpha(newAlpha);
+            if (mBattery != null) mBattery.setAlpha(newAlphaBC);
+            if (mClock != null) mClock.setAlpha(newAlphaBC);
+            if (mCenterClock != null) mCenterClock.setAlpha(newAlphaBC);
+            if (mLeftClock != null) mLeftClock.setAlpha(newAlphaBC);
+            if (mBlissLogo != null) mBlissLogo.setAlpha(newAlphaBC);
         }
     }
 }

DesolationRebase/frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -54,6 +54,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     }
 
     public ObjectAnimator animateTransitionTo(View v, float toAlpha) {
+        if (v == null) return null;
         return ObjectAnimator.ofFloat(v, "alpha", v.getAlpha(), toAlpha);
     }
 
@@ -102,12 +103,12 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             anims.start();
             mCurrentAnimation = anims;
         } else {
-            mLeftSide.setAlpha(newAlpha);
-            mStatusIcons.setAlpha(newAlpha);
-            mSignalCluster.setAlpha(newAlpha);
-            mBattery.setAlpha(newAlphaBC);
-            mClock.setAlpha(newAlphaBC);
-            mDesoLogo.setAlpha(newAlphaBC);
+            if (mLeftSide != null) mLeftSide.setAlpha(newAlpha);
+            if (mStatusIcons != null) mStatusIcons.setAlpha(newAlpha);
+            if (mSignalCluster != null) mSignalCluster.setAlpha(newAlpha);
+            if (mBattery != null) mBattery.setAlpha(newAlphaBC);
+            if (mClock != null) mClock.setAlpha(newAlphaBC);
+            if (mDesoLogo != null) mDesoLogo.setAlpha(newAlphaBC);
         }
     }
 }